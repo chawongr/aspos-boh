98c098a4df9802cf5cd4403826f29f7801e48d55
bbbaipooo <baipoo_B@hotmail.com>
2024-12-28 13:44:02 +0700

    fix(system):tab main

---
+++ aspos-boh-frontend/src/pages/pos-configuration/system/system/main/blocks/GeneralMain.tsx
@@ -0,0 +1,279 @@
+import { useState } from 'react';
+import { KeenIcon } from '@/components';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+
+const GeneralMain = () => {
+  return (
+    <div>
+      <div className="card mb-10">
+        <div className="card-header" id="webhooks">
+          <h3 className="card-title">Main</h3>
+        </div>
+        <div className="card-body grid gap-5">
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">Store No</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter Store Number" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5 h-10">
+            <span className="form-label max-w-32 w-full">Store Name</span>
+            <div className="grow min-w-48">
+              <span className="text-2sm text-gray-700 w-full">James Capenter</span>
+                {/* <input className="input w-full" type="text" placeholder="Enter Combo Code" value="CB01 Combo1" /> */}
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5 h-10">
+            <span className="form-label max-w-32 w-full">Version</span>
+            <div className="grow min-w-48">
+            <span className="text-2sm text-gray-700 w-full">3.2.0.0.1</span>
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5 h-10">
+            <span className="form-label max-w-32 w-full">Business Date</span>
+            <div className="grow min-w-48">
+            <span className="text-2sm text-gray-700 w-full">12/11/2024</span>
+            </div>
+          </div>
+
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Decimal Point</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="dp0">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                          <SelectItem value="dp0">0</SelectItem>
+                          <SelectItem value="dp1">1</SelectItem>
+                          <SelectItem value="dp2">2</SelectItem>
+                          <SelectItem value="dp3">3</SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">Amount Due Rounding</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" />
+            </div>
+          </div>
+
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Default Tender Media</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="td1">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                          <SelectItem value="td1">Tender</SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+          
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Default Sales Type</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="st1">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                          <SelectItem value="st1">sale type 1</SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Default Waste</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="w1">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                          <SelectItem value="w1">waste 1</SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Approver User</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="user1">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                          <SelectItem value="user1">user 1</SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32"></label>
+              <div className="switch switch-sm grow flex justify-start">
+                <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+              </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">Clear training data every</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter Number of Days" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">API URL</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter URL" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">ERP URL</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter URL" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">POS Host URL1</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter URL" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">POS Host URL2</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter URL" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">POS Host URL3</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter URL" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">Backup DB Name</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter DB Name" />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">Call Center</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" placeholder="Enter Cal Center" />
+            </div>
+          </div>
+
+          <div className="flex items-baseline flex-wrap gap-2.5">
+              <label className="form-label max-w-32">Language</label>
+              <div className="grid gap-5 grow items-start">
+                  <Select defaultValue="user1">
+                      <SelectTrigger size="md">
+                          <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="user1">
+                          <img src="" className="inline-block size-3.5 rounded-full mr-2" alt="{currentLanguage.label}"/>
+                            English
+                        </SelectItem>
+                        <SelectItem value="user2">
+                          <img src="" className="inline-block size-3.5 rounded-full mr-2" alt="{currentLanguage.label}"/>
+                            Franch
+                        </SelectItem>
+                      </SelectContent>
+                  </Select>
+              </div>
+          </div>
+          
+
+          {/* <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">24hr Time</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">On - Weight in Pounds</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">On - Use Auto business date</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">On - Pickup control by employee</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">Perform inventory closing during end of day process</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">Do not allow inventory receipt without purchase order</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div>
+
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label w-full">Shutdown computer after end of day process</span>
+            <div className="switch switch-sm grow flex justify-end">
+              <input type="checkbox" value="1" name="check" defaultChecked readOnly />
+            </div>
+          </div> */}
+
+        </div>
+      </div>
+      {/* <div className="card mt-7">
+        <div className="card-body grid gap-5">
+          <div className="flex items-center flex-wrap lg:flex-nowrap gap-2.5">
+            <span className="form-label max-w-32 w-full">License</span>
+            <div className="grow min-w-48">
+              <input className="input w-full" type="text" />
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export { GeneralMain };
---
+++ aspos-boh-frontend/src/pages/pos-configuration/system/system/option/SystemOptionPage.tsx
@@ -0,0 +1,48 @@
+import { Fragment } from 'react';
+
+import { Container } from '@/components/container';
+import {
+  Toolbar,
+  ToolbarActions,
+  ToolbarDescription,
+  ToolbarHeading,
+  ToolbarPageTitle
+} from '@/partials/toolbar';
+
+import { SystemOptionContent } from '.';
+import { useLayout } from '@/providers';
+import { PageNavbar } from '@/pages/pos-configuration/system';
+
+const SystemOptionPage = () => {
+  const { currentLayout } = useLayout();
+
+  return (
+    <Fragment>
+
+      {currentLayout?.name === 'demo1-layout' && (
+        <Container>
+          <Toolbar>
+            <ToolbarHeading>
+              {/* <ToolbarPageTitle /> */}
+              <h1 className="text-xl font-medium leading-none text-gray-900">System</h1>
+              <ToolbarDescription>System POS Management</ToolbarDescription>
+            </ToolbarHeading>
+            {/* <ToolbarActions>
+              <div className="btn btn-sm btn-primary">
+                Save
+              </div>
+            </ToolbarActions> */}
+          </Toolbar>
+        </Container>
+      )}
+
+      <PageNavbar />
+
+      <Container>
+        <SystemOptionContent />
+      </Container>
+    </Fragment>
+  );
+};
+
+export { SystemOptionPage };
